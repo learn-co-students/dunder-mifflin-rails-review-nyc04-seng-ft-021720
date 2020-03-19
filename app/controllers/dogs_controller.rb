f5c596bbc8fac6668acbae73abf230755e9bb23e
schimajo75 <schimmy@gmail.com>
2020-03-19 17:18:58 -0400

    hot KEVIN

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,14 @@
 class DogsController < ApplicationController
+    def index
+        @dogs = Dog.all.sort_by { |dog| dog.employees.count * -1}
+        # byebug
+        #@dogs[1].employees.count
+        # @dogs.sort_by { |dog| dog.employees.count }
+    end
+    
+    def show
+        @dog = Dog.find(params[:id])
+    end
+
+ 
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,7 @@
 class Employee < ApplicationRecord
+    belongs_to :dog
+    validates :first_name, presence: {message: "please enter a name"}
+    validates :last_name, presence: {message: "please enter a name"}
+    validates :title, uniqueness: {message: "already exists"}
+    validates :alias, uniqueness: {message: "already exists"}
 end
